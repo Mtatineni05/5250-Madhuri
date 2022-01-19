cc29a3243e890a305e242b1d24d1c4c102aab294
Mtatineni05 <madhuri.tatineni22@yahoo.com>
2022-01-18 19:17:35 -0800

    Added Comments to ItemModel.cs

--- Mine/Mine/Models/ItemModel.cs
+++ Mine/Mine/Models/ItemModel.cs
@@ -4,8 +4,11 @@ namespace Mine.Models
 {
     public class ItemModel
     {
+        //The ID for an item
         public string Id { get; set; }
+        //The Display text for an item
         public string Text { get; set; }
+        //The description for an item
         public string Description { get; set; }
         //The Value of item +9 Damage
         public int Value { get; set; } = 0;
